9dc3064a69e081d1a8d5384cca8bb83c703d8d39
Alta06 <antoine6006@gmail.com>
2023-03-10 23:27:30 +0000

    feat: answer-with-atomic-number

--- element.sh
+++ element.sh
@@ -5,5 +5,15 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 if [[ ! $1 ]]
 then
 echo "Please provide an element as an argument."
+fi
 
+if [[ $1 == [0-9]* ]]
+then
+NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number=$1")
+SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number=$1")
+TYPE=$($PSQL "SELECT type FROM types INNER JOIN properties USING(type_id) WHERE atomic_number=$1")
+MASS=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number=$1")
+MELTING_POINT=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$1")
+BOILING_POINT=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$1")
+echo "The element with atomic number $1 is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
 fi
