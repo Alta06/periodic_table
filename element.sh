aa4c19351609270690f5ce8936f558fc0389efda
Alta06 <antoine6006@gmail.com>
2023-03-13 14:13:39 +0000

    refactor: answer-with-name

--- element.sh
+++ element.sh
@@ -1,14 +1,8 @@
 #! /bin/bash
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
-
-if [[ ! $1 ]]
-then
-echo "Please provide an element as an argument."
-
-elif [[ $1 == [0-9]* ]]
-  then
-  NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number=$1")
+TEST_ATOMIC_NUMBER() {
+ NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number=$1")
   SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number=$1")
   TYPE=$($PSQL "SELECT type FROM types INNER JOIN properties USING(type_id) WHERE atomic_number=$1")
   MASS=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number=$1")
@@ -20,9 +14,10 @@ elif [[ $1 == [0-9]* ]]
       else
       echo "The element with atomic number $1 is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
     fi
-elif [[ $1 =~ [A-Za-z]{3,} ]]
-then
-#gérer le cas de réponse avec le nom de l'élément
+}
+
+TEST_SYMBOL() {
+  #gérer le cas de réponse avec le nom de l'élément
   ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE name='$1'")
   SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE name='$1'")
   TYPE=$($PSQL "SELECT type FROM types INNER JOIN properties USING(type_id) INNER JOIN elements USING(atomic_number) WHERE name='$1'")
@@ -35,10 +30,10 @@ then
       else
       echo "The element with atomic number $ATOMIC_NUMBER is $1 ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $1 has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
     fi
+}
 
-elif [[ $1 =~ [A-Z]{1,2} ]]
-  then
-  NAME=$($PSQL "SELECT name FROM elements WHERE symbol='$1'")
+TEST_NAME() {
+   NAME=$($PSQL "SELECT name FROM elements WHERE symbol='$1'")
   ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE symbol='$1'")
   TYPE=$($PSQL "SELECT type FROM types INNER JOIN properties USING(type_id) INNER JOIN elements USING(atomic_number) WHERE symbol='$1'")
   MASS=$($PSQL "SELECT atomic_mass FROM properties INNER JOIN elements USING(atomic_number) WHERE symbol='$1'")
@@ -50,7 +45,23 @@ elif [[ $1 =~ [A-Z]{1,2} ]]
       else
       echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($1). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
     fi
+}
 
+if [[ ! $1 ]]
+  then
+  echo "Please provide an element as an argument."
+
+elif [[ $1 == [0-9]* ]]
+  then
+  TEST_ATOMIC_NUMBER $1
+
+elif [[ $1 =~ [A-Za-z]{3,} ]]
+  then
+  TEST_SYMBOL $1
+
+elif [[ $1 =~ [A-Z]{1,2} ]]
+  then
+  TEST_NAME $1
 
 fi
 
