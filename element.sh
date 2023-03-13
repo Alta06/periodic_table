126ffcefa79f167cadad7d6985188c87c172411c
Alta06 <antoine6006@gmail.com>
2023-03-13 14:04:44 +0000

    feat:answer-with-name

--- element.sh
+++ element.sh
@@ -20,6 +20,21 @@ elif [[ $1 == [0-9]* ]]
       else
       echo "The element with atomic number $1 is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
     fi
+elif [[ $1 =~ [A-Za-z]{3,} ]]
+then
+#gérer le cas de réponse avec le nom de l'élément
+  ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE name='$1'")
+  SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE name='$1'")
+  TYPE=$($PSQL "SELECT type FROM types INNER JOIN properties USING(type_id) INNER JOIN elements USING(atomic_number) WHERE name='$1'")
+  MASS=$($PSQL "SELECT atomic_mass FROM properties INNER JOIN elements USING(atomic_number) WHERE name='$1'")
+  MELTING_POINT=$($PSQL "SELECT melting_point_celsius FROM properties INNER JOIN elements USING(atomic_number) WHERE name='$1'")
+  BOILING_POINT=$($PSQL "SELECT boiling_point_celsius FROM properties INNER JOIN elements USING(atomic_number) WHERE name='$1'")
+    if [[ -z $ATOMIC_NUMBER ]]
+      then 
+      echo "I could not find that element in the database."
+      else
+      echo "The element with atomic number $ATOMIC_NUMBER is $1 ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $1 has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+    fi
 
 elif [[ $1 =~ [A-Z]{1,2} ]]
   then
@@ -36,21 +51,7 @@ elif [[ $1 =~ [A-Z]{1,2} ]]
       echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($1). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
     fi
 
-elif [[ $1 =~ [A-Z]{3,} ]]
-then
-#gérer le cas de réponse avec le nom de l'élément
-  ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE name='$1'")
-  SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE name='$1'")
-  TYPE=$($PSQL "SELECT type FROM types INNER JOIN properties USING(type_id) INNER JOIN elements USING(atomic_number) WHERE name='$1'")
-  MASS=$($PSQL "SELECT atomic_mass FROM properties INNER JOIN elements USING(atomic_number) WHERE name='$1'")
-  MELTING_POINT=$($PSQL "SELECT melting_point_celsius FROM properties INNER JOIN elements USING(atomic_number) WHERE name='$1'")
-  BOILING_POINT=$($PSQL "SELECT boiling_point_celsius FROM properties INNER JOIN elements USING(atomic_number) WHERE name='$1'")
-    if [[ -z $ATOMIC_NUMBER ]]
-      then 
-      echo "I could not find that element in the database."
-      else
-      echo "The element with atomic number $ATOMIC_NUMBER is $1 ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $1 has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
-    fi
+
 fi
 
 
